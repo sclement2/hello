6786e87189f219471274e3570940bb4c9f33ae1e
sclement2 <sclement2@gmail.com>
2019-11-25 21:54:23 -0800

    Added a package that I  created to the "go" file

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sclement2/stringutil"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
